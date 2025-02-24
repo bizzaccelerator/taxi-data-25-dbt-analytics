1d356a497f24dfa918f87cb24164287d2c0f4cd0
Jobert Gutierrez <jobert.gutierrez@gmail.com>
2025-02-24 17:45:55 +0000

    stg_Fhv model created

---
+++ dbt-interaction/models/staging/stg_fhv_tripdata.sql
@@ -0,0 +1,34 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+with tripdata as (
+    select *
+    from {{ source("staging","fhv_taxi_25_non_partition")}}
+    where dispatching_base_num is not null
+)
+
+select 
+    -- Identifiers
+    {{ dbt_utils.generate_surrogate_key(["Affiliated_base_number", "pickup_datetime"]) }} as tripid,
+    {{ dbt.safe_cast("PUlocationID", api.Column.translate_type("integer")) }} as pulocationid,
+    {{ dbt.safe_cast("DOlocationID", api.Column.translate_type("integer")) }} as dolocationid,
+    
+    -- Trip info
+    dispatching_base_num,
+    {{ dbt.safe_cast("SR_Flag,", api.Column.translate_type("integer"))}} as sr_flag,
+    Affiliated_base_number,
+
+    -- Timestamps
+    cast(pickup_datetime as timestamp) as pickup_datetime
+    cast(dropOff_datetime as timestamp) as dropoff_datetime
+
+from trips_data
+
+{% if var('is_test_run', default=true) %}
+
+    limit 100
+
+{% endif %}
