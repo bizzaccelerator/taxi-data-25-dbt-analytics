2bb84696667aa23646e8edc3b1fec156e36853c0
Jobert Gutierrez <jobert.gutierrez@gmail.com>
2025-02-25 14:39:11 +0000

    fct_fhv_monthly_zone_traveltime_p90 model created and adjusted.

--- dbt-interaction/models/core/fct_fhv_monthly_zone_traveltime_p90.sql
+++ dbt-interaction/models/core/fct_fhv_monthly_zone_traveltime_p90.sql
@@ -1,9 +1,3 @@
-{{
-    config(
-        materialized='table'
-    )
-}}
-
 with trips_data as (
     select 
         dispatching_base_num,
@@ -27,9 +21,12 @@ p90_trip_duration as (
         pickup_month,
         pulocationid,
         dolocationid,
-        APPROX_QUANTILES(trip_duration, 100)[OFFSET(90)] as trip_duration_p90
-    from trips_data
-    group by pickup_year, pickup_month, pulocationid, dolocationid
+        trip_duration,  -- Include trip_duration
+        PERCENT_RANK() OVER (
+            PARTITION BY pickup_year, pickup_month, pulocationid, dolocationid
+            ORDER BY trip_duration
+        ) AS trip_duration_p90
+    from trips_data  
 )
 
 select 
@@ -41,4 +38,5 @@ on t.pickup_year = p.pickup_year
 and t.pickup_month = p.pickup_month
 and t.pulocationid = p.pulocationid
 and t.dolocationid = p.dolocationid
+and t.trip_duration = p.trip_duration  -- Ensure trip_duration is also part of the join
 order by t.pickup_year, t.pickup_month, t.trip_duration
